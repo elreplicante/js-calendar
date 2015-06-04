d3cbfda4efc5f4f9be9e9ffb7f4ce3cbd4cef4ec
trikitrok <trikitrok@gmail.com>
2015-06-04 15:55:02 +0200

    Removed some duplication in tests

--- spec/DateRangeSpec.js
+++ spec/DateRangeSpec.js
@@ -1,37 +1,31 @@
 'use strict';
 
 describe('DateRange', function() {
-  var clock = Clock(),
-    dateRange;
+  var clock, dateRange;
 
-  it('should show the current week on creation ', function() {
+  beforeEach(function() {
+    clock = Clock();
     spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
     dateRange = DateRange(clock, dateManipulations);
+  });
 
+  it('should show the current week on creation ', function() {
     expectThatDateRange(dateRange).startsOn("2014-11-10").andEndsOn("2014-11-16");
   });
 
   it('should show the current month when a month period is selected', function() {
-    spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
-    dateRange = DateRange(clock, dateManipulations);
-
     dateRange.useMonth();
 
     expectThatDateRange(dateRange).startsOn("2014-11-01").andEndsOn("2014-11-30");
   });
 
   it('should show the current year when a year period is selected', function() {
-    spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
-    dateRange = DateRange(clock, dateManipulations);
-
     dateRange.useYear();
 
     expectThatDateRange(dateRange).startsOn("2014-01-01").andEndsOn("2014-12-31");
   });
 
-  it('should show the current week when a week period is selected back', function() {
-    spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
-    dateRange = DateRange(clock, dateManipulations);
+  it('should show the current week when a week period is selected', function() {
     dateRange.useYear();
 
     dateRange.useWeek();
@@ -41,7 +35,7 @@ describe('DateRange', function() {
 
   function expectThatDateRange(dateRange) {
     return {
-      startsOn: function (startDate) {
+      startsOn: function(startDate) {
         return {
           andEndsOn: function(endDate) {
             expect(dateRange.startDate()).toEqual(moment(startDate).toDate());
