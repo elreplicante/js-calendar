27c77c46a95140c65da605c1c1fc7f63c810e8b2
trikitrok <trikitrok@gmail.com>
2015-06-04 13:09:50 +0200

    It should show the current month when a month period is selected

--- spec/DateRangeSpec.js
+++ spec/DateRangeSpec.js
@@ -1,21 +1,39 @@
 'use strict';
 
-describe('DateRange', function () {
-  it('should show the current week on creation ', function () {
-    
-    var clock = {
-      currentDate: function(){
-        return new Date();
-      }
-    };
+describe('DateRange', function() {
+  it('should show the current week on creation ', function() {
+    var dateRange,
+      clock = {
+        currentDate: function() {
+          return new Date();
+        }
+      };
 
-    spyOn(clock, 'currentDate').and.returnValue("2015-06-11");
+    spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
 
-    var dateRange = new DateRange(clock);
-    var startDate = moment("2015-06-08").toDate();
-    var endDate = moment("2015-06-14").toDate();
+    dateRange = DateRange(clock);
 
-    expect(dateRange.startDate()).toEqual(startDate);
-    expect(dateRange.endDate()).toEqual(endDate);
+    expectDateRange("2014-11-10", "2014-11-16", dateRange);
   });
+
+  it('should show the current month when a month period is selected', function() {
+    var dateRange,
+      clock = {
+        currentDate: function() {
+          return new Date();
+        }
+      };
+      spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
+
+      dateRange = DateRange(clock);
+
+      dateRange.useMonth();
+
+     expectDateRange("2014-11-01", "2014-11-30", dateRange);
+  });
+
+  function expectDateRange(startDate, endDate, dateRange) {
+    expect(dateRange.startDate()).toEqual(moment(startDate).toDate());
+    expect(dateRange.endDate()).toEqual(moment(endDate).toDate());
+  }
 });
--- src/DateRange.js
+++ src/DateRange.js
@@ -1,29 +1,47 @@
-function DateRange(clock) {
+var DateRange = (function() {
+  return function(clock) {
+    var startDate, endDate, period = "WEEK";
 
-  this.clock = clock;
-  this._startDate;
-  this._endDate;
+    updateRange(clock.currentDate());
 
-  this.monday = function(date) {
-    return moment(date).day("Monday");
-  }
+    return {
+      startDate: function() {
+        return startDate;
+      },
+      endDate: function() {
+        return endDate;
+      },
+      useMonth: function() {
+        period = "MONTH";
+        updateRange(clock.currentDate());
+      }
+    };
 
-  this.sunday = function(date) {
-    return this.monday(date).add(6, "days");
-  }
+    function monday(date) {
+      return moment(date).day("Monday");
+    }
 
-  this.updateRange = function(currentDate) {
-    this._startDate = this.monday(currentDate).toDate();
-    this._endDate = this.sunday(currentDate).toDate();
-  }
+    function sunday(date) {
+      return monday(date).add(6, "days");
+    }
 
-  this.updateRange(this.clock.currentDate());
+    function firstDayOfMonth(date) {
+      return moment(date).date(1);
+    }
 
-  this.startDate = function() {
-    return this._startDate;
-  }
+    function lastDayOfMonth(date) {
+      var daysInMonth = moment(date).daysInMonth();
+      return firstDayOfMonth(date).add(daysInMonth - 1, "days");
+    }
 
-  this.endDate = function() {
-    return this._endDate;
-  }
-}
+    function updateRange(currentDate) {
+      if(period === "WEEK") {
+        startDate = monday(currentDate).toDate();
+        endDate = sunday(currentDate).toDate();
+      } else {
+        startDate = firstDayOfMonth(currentDate).toDate();
+        endDate = lastDayOfMonth(currentDate).toDate();
+      }
+    }
+  };
+})();
