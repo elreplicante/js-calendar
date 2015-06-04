f2c63ccdd5a5782a89aae4ae15c813f72b2c700b
trikitrok <trikitrok@gmail.com>
2015-06-04 16:02:04 +0200

    Rearranged tests to group them by behaviour and remove some duplication from their names

--- spec/DateRangeSpec.js
+++ spec/DateRangeSpec.js
@@ -3,34 +3,43 @@
 describe('DateRange', function() {
   var clock, dateRange;
 
-  beforeEach(function() {
-    clock = Clock();
-    spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
-    dateRange = DateRange(clock, dateManipulations);
+  describe('on creation', function() {
+    it('should show the current week', function() {
+      clock = Clock();
+      spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
+      dateRange = DateRange(clock, dateManipulations);
+
+      expectThatDateRange(dateRange).startsOn("2014-11-10").andEndsOn("2014-11-16");
+    });
   });
 
-  it('should show the current week on creation ', function() {
-    expectThatDateRange(dateRange).startsOn("2014-11-10").andEndsOn("2014-11-16");
-  });
+  describe('when a new period is selected', function() {
+    beforeEach(function() {
+      clock = Clock();
+      spyOn(clock, 'currentDate').and.returnValue("2014-11-11");
+      dateRange = DateRange(clock, dateManipulations);
+    });
 
-  it('should show the current month when a month period is selected', function() {
-    dateRange.useMonth();
+    it('should show the current month in case of a month', function() {
+      dateRange.useMonth();
 
-    expectThatDateRange(dateRange).startsOn("2014-11-01").andEndsOn("2014-11-30");
-  });
+      expectThatDateRange(dateRange).startsOn("2014-11-01").andEndsOn("2014-11-30");
+    });
 
-  it('should show the current year when a year period is selected', function() {
-    dateRange.useYear();
+    it('should show the current year in case of a year', function() {
+      dateRange.useYear();
 
-    expectThatDateRange(dateRange).startsOn("2014-01-01").andEndsOn("2014-12-31");
-  });
+      expectThatDateRange(dateRange).startsOn("2014-01-01").andEndsOn("2014-12-31");
+    });
 
-  it('should show the current week when a week period is selected', function() {
-    dateRange.useYear();
+    it('should show the current week in case of a week', function() {
+      dateRange.useYear();
 
-    dateRange.useWeek();
+      dateRange.useWeek();
 
-    expectThatDateRange(dateRange).startsOn("2014-11-10").andEndsOn("2014-11-16");
+      expectThatDateRange(dateRange).startsOn("2014-11-10").andEndsOn("2014-11-16");
+    });
+
   });
 
   function expectThatDateRange(dateRange) {
