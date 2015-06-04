781df6f24e7734db446c491f537c1fb246d1a4dc
trikitrok <trikitrok@gmail.com>
2015-06-04 14:17:12 +0200

    Gave named to period types constructor functions

--- src/DateRange.js
+++ src/DateRange.js
@@ -1,55 +1,9 @@
 var DateRange = (function() {
   return function(clock) {
     var startDate, endDate,
-      month = (function() {
-        return {
-          updateRange: function(currentDate) {
-            setRange(firstDayOfMonth(currentDate), lastDayOfMonth(currentDate));
-          }
-        };
-
-        function firstDayOfMonth(date) {
-          return moment(date).date(1);
-        }
-
-        function lastDayOfMonth(date) {
-          var daysInMonth = moment(date).daysInMonth();
-          return firstDayOfMonth(date).add(daysInMonth - 1, "days");
-        }
-      })(),
-
-      week = (function() {
-        return {
-          updateRange: function(currentDate) {
-            setRange(monday(currentDate), sunday(currentDate));
-          }
-        };
-
-        function monday(date) {
-          return moment(date).day("Monday");
-        }
-
-        function sunday(date) {
-          return monday(date).add(6, "days");
-        }
-      })(),
-
-      year = (function() {
-        return {
-          updateRange: function(currentDate) {
-            setRange(firstDayOfYear(currentDate), lastDayOfYear(currentDate));
-          }
-        };
-
-        function firstDayOfYear(date) {
-          return moment(date).dayOfYear(1);
-        }
-
-        function lastDayOfYear(date) {
-          return firstDayOfYear(date).add(1, "years").add(-1, "days");
-        }
-      })(),
-
+      month = Month(),
+      week = Week(),
+      year = Year(),
       period = week;
 
     updateRange();
@@ -78,6 +32,55 @@ var DateRange = (function() {
     function setRange(newStartDate, newEndDate) {
       startDate = newStartDate.toDate();
       endDate = newEndDate.toDate();
+    }
+
+    function Month() {
+      return {
+        updateRange: function(currentDate) {
+          setRange(firstDayOfMonth(currentDate), lastDayOfMonth(currentDate));
+        }
+      };
+
+      function firstDayOfMonth(date) {
+        return moment(date).date(1);
+      }
+
+      function lastDayOfMonth(date) {
+        var daysInMonth = moment(date).daysInMonth();
+        return firstDayOfMonth(date).add(daysInMonth - 1, "days");
+      }
+    }
+
+    function Week() {
+      return {
+        updateRange: function(currentDate) {
+          setRange(monday(currentDate), sunday(currentDate));
+        }
+      };
+
+      function monday(date) {
+        return moment(date).day("Monday");
+      }
+
+      function sunday(date) {
+        return monday(date).add(6, "days");
+      }
+    }
+
+    function Year() {
+      return {
+        updateRange: function(currentDate) {
+          setRange(firstDayOfYear(currentDate), lastDayOfYear(currentDate));
+        }
+      };
+
+      function firstDayOfYear(date) {
+        return moment(date).dayOfYear(1);
+      }
+
+      function lastDayOfYear(date) {
+        return firstDayOfYear(date).add(1, "years").add(-1, "days");
+      }
     }
   };
 })();
