63ba9ac815f23b7af787cef725e2ae6c1d5ed85f
trikitrok <trikitrok@gmail.com>
2015-06-04 15:38:42 +0200

    Inlined some helpers

--- src/DateRange.js
+++ src/DateRange.js
@@ -37,49 +37,34 @@ var DateRange = (function() {
     function Month(dateFns) {
       return {
         updateRange: function(currentDate) {
-          setRange(firstDayOfMonth(currentDate), lastDayOfMonth(currentDate));
+          setRange(
+            dateFns.firstDayOfMonth(currentDate),
+            dateFns.lastDayOfMonth(currentDate)
+          );
         }
       };
-
-      function firstDayOfMonth(date) {
-        return dateFns.firstDayOfMonth(date);
-      }
-
-      function lastDayOfMonth(date) {
-        return dateFns.lastDayOfMonth(date);
-      }
     }
 
     function Week(dateFns) {
       return {
         updateRange: function(currentDate) {
-          setRange(monday(currentDate), sunday(currentDate));
+          setRange(
+            dateFns.monday(currentDate),
+            dateFns.sunday(currentDate)
+          );
         }
       };
-
-      function monday(date) {
-        return dateFns.monday(date);
-      }
-
-      function sunday(date) {
-        return dateFns.sunday(date);
-      }
     }
 
     function Year(dateFns) {
       return {
         updateRange: function(currentDate) {
-          setRange(firstDayOfYear(currentDate), lastDayOfYear(currentDate));
+          setRange(
+            dateFns.firstDayOfYear(currentDate),
+            dateFns.lastDayOfYear(currentDate)
+          );
         }
       };
-
-      function firstDayOfYear(date) {
-        return dateFns.firstDayOfYear(date);
-      }
-
-      function lastDayOfYear(date) {
-        return dateFns.lastDayOfYear(date);
-      }
     }
   };
 })();
