033ff994fba23e24997a4071cca4153cd116248d
trikitrok <trikitrok@gmail.com>
2015-06-05 01:46:45 +0200

    Moved public API to the top of the module

--- src/DateManipulations.js
+++ src/DateManipulations.js
@@ -6,6 +6,18 @@ var dateManipulations = (function() {
     MONTHS = "months",
     WEEKS = "weeks";
 
+  return {
+    oneWeekAheadOf: dateOf(ahead(1, WEEKS)),
+    oneMonthAheadOf: dateOf(ahead(1, MONTHS)),
+    oneYearAheadOf: dateOf(ahead(1, YEARS)),
+    oneWeekBackFrom: dateOf(back(1, WEEKS)),
+    oneMonthBackFrom: dateOf(back(1, MONTHS)),
+    oneYearBackFrom: dateOf(back(1, YEARS)),
+    monthPeriodContaining: monthPeriodContaining,
+    yearPeriodContaining: yearPeriodContaining,
+    weekPeriodContaining: weekPeriodContaining
+  };
+
   function firstDayOfYear(date) {
     return moment(date).dayOfYear(1);
   }
@@ -69,16 +81,4 @@ var dateManipulations = (function() {
       endDate: lastDayOfWeek(date).toDate()
     };
   }
-
-  return {
-    oneWeekAheadOf: dateOf(ahead(1, WEEKS)),
-    oneMonthAheadOf: dateOf(ahead(1, MONTHS)),
-    oneYearAheadOf: dateOf(ahead(1, YEARS)),
-    oneWeekBackFrom: dateOf(back(1, WEEKS)),
-    oneMonthBackFrom: dateOf(back(1, MONTHS)),
-    oneYearBackFrom: dateOf(back(1, YEARS)),
-    monthPeriodContaining: monthPeriodContaining,
-    yearPeriodContaining: yearPeriodContaining,
-    weekPeriodContaining: weekPeriodContaining
-  };
 })();
